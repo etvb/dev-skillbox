00c691018242692f7864d7b7e0520bb8125c432d
linux-kbl <edgardo.torres@hotmail.comls>
2020-08-19 21:56:32 -0500

    put the texts

--- pages/index.vue
+++ pages/index.vue
@@ -23,6 +23,138 @@
     </div>
     <!-- ./ Jumbotron -->
 
+    <!-- NEW PART -->
+    <div class="section">
+      <div class="container">
+        <h3 class="title title25"> 
+          HOW IT WORKS
+        </h3>
+      </div>
+      <a href="#">
+        <div class="container box">
+          <div class="columns is-mobile">
+            <div class="column">
+              <p class="title25 is-4">FIND</p>
+              <p>Search for the language you want to learn and choose a teacher who suits you best.</p>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </a>
+
+      <a href="#">
+        <div class="container box">
+          <div class="columns is-mobile">
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+              </figure>
+            </div>
+            <div class="column">
+              <p class="title25 is-4">Book</p>
+              <p>Check the teacher‘s availability and book your one-on-one language class right away.</p>
+            </div>
+          </div>
+        </div>
+      </a>
+
+      <a href="#">
+        <div class="container box">
+          <div class="columns is-mobile">
+            <div class="column">
+              <p class="title25 is-4">START</p>
+              <p>Join your teacher in your one-on-one video class from wherever you feel comfortable. Start unboxing your skillzz...</p>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <!-- COMMUNITY -->
+    <div class="section">
+      <div class="container">
+        <h3 class="title title25"> 
+          JOIN OUR COMMUNITY
+        </h3>
+      </div>
+      <a href="#">
+        <div class="container box">
+          <div class="columns is-mobile">
+            <div class="column">
+              <p class="title25 is-4">STUDENT</p>
+              <ul>
+                <li>Be flexible: Book classes whenever you have time.</li>
+                <li>Profit the most: Join a one-on-one class for a reasonable price.</li>
+                <li>No rush to your class: Just start your device and join the class wherever you are.</li>
+              </ul>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </a>
+
+      <a href="#">
+        <div class="container box">
+          <div class="columns is-mobile">
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+              </figure>
+            </div>
+            <div class="column">
+              <p class="title25 is-4">TEACHER</p>
+              <ul>
+                <li>Earn money flexibly: Teach online from home and on your schedule.</li>
+                <li>Motivated learners: Work with motivated students who have picked you as their teacher.</li>
+                <li>Be creative: Design your own curriculum and teach the way you want.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </a>
+
+      <a href="#">
+        <div class="container box">
+          <div class="columns is-mobile">
+            <div class="column">
+              <p class="title25 is-4">DO YOU HAVE A QUESTION?</p>
+              <p>Please contact us.</p>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="container box">
+          <div class="columns is-mobile">
+            <div class="column">
+              <p class="title25 is-4">DO YOU LIKE OUR PLATFORM?</p>
+              <p>Tell a friend.</p>
+            </div>
+            <div class="column" />
+          </div>
+        </div>
+      </a>
+    </div>
+    <!-- /COMMUNITY -->
+
     <!-- Description -->
     <div class="section">
       <div class="container">
@@ -175,8 +307,14 @@
     </nuxt-link>
   </div>
 </template>
-<style lang="sass">
+<style lang="sass" scoped>
   @import "~/assets/css/_media-queries.sass"
+  .title25
+    // font-family: 'Calibri'
+    font-size: 25px
+    font-weight: 600
+    
+
   .-home-jumbotron
     background-image: url('/learning-languages.jpeg')
     background-position: center
